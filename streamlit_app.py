48d46be0032af7285f5c492e0ba6338fffd41616
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-01 16:40:24 +0200

    Update streamlit_app.py
    Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -18,7 +18,6 @@ st.write('The name of your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-
 st.dataframe(data=my_dataframe, use_container_width = True)
 #pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
