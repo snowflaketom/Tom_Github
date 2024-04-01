79877938c564c6c6bc1455a6a5457b5d3da86905
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-01 16:28:32 +0200

    Update streamlit_app.py
    Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
+import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
-#import pandas as pd
 
 # Write directly to the app
 st.title("Example Streamlit App 2 :crown:")
