bcfd0e471400b3b8200ff306526c3d8b65e93ecc
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-01 16:42:33 +0200

    Update streamlit_app.py
    my1

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,13 @@ st.write('The name of your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width = True)
+my1_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my1_dataframe, use_container_width = True)
 #pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 st.stop
 
-ing_list=st.multiselect('Choose up to 5 ing.',my_dataframe,max_selections=5)
+ing_list=st.multiselect('Choose up to 5 ing.',my1_dataframe,max_selections=5)
 
 if ing_list:
     ing_string = ''
