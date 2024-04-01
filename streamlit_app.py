5bda3f91e9fa08413a61903ade287953545e37e4
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-01 16:27:11 +0200

    Update streamlit_app.py
    Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-pd_df = my_dataframe.to_pandas()
-st.dataframe[pd_df]
+#pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 st.stop
 
-ing_list=st.multiselect('Choose up to 5 ing.',my_dataframe,max_selections=5) 
+ing_list=st.multiselect('Choose up to 5 ing.',my_dataframe,max_selections=5)
 
 if ing_list:
     ing_string = ''
