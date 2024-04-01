d5bc28262fcf0cbdaff422980303ebf4e88eccae
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-01 16:06:09 +0200

    Update streamlit_app.py
    Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
+st.stop
 
 ing_list=st.multiselect('Choose up to 5 ing.',my_dataframe,max_selections=5) 
 
