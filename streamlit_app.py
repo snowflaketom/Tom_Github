16fa377b264a08b38904388b7ad63f531f63b841
snowflaketom <165655943+snowflaketom@users.noreply.github.com>
2024-04-06 10:09:23 +0200

    Update streamlit_app.py
    Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-# import pandas as pd
+import pandas as pd
 import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
